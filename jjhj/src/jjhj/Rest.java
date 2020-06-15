b484c2d3c14079b31d7bbae64d21bd4935a0e30b
bharathysivakumar <bharathysivakumar6@gmail.com>
2020-06-15 19:31:27 -0400

    hii

---
+++ jjhj/src/jjhj/Rest.java
@@ -0,0 +1,18 @@
+package jjhj;
+
+import java.util.Scanner;
+
+public class Rest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
